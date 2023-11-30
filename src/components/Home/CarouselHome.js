e7e358d0693dbb840e19a86b5f34968ad368c59c
tuan24072002 <44106906+tuan24072002@users.noreply.github.com>
2023-11-30 10:42:14 +0700

    new update 30/11/2023

--- src/components/Home/CarouselHome.js
+++ src/components/Home/CarouselHome.js
@@ -5,8 +5,11 @@ import carousel2 from '../../assets/images/carousel-2.jpg'
 import carousel3 from '../../assets/images/carousel-3.jpg'
 import carousel4 from '../../assets/images/carousel-4.jpg'
 import carousel5 from '../../assets/images/carousel-5.jpg'
+import carousel6 from '../../assets/images/carousel-6.jpg'
+import carousel7 from '../../assets/images/carousel-7.jpg'
+import carousel8 from '../../assets/images/carousel-8.jpg'
 const CarouselHome = () => {
-    const poster = [carousel1, carousel2, carousel3, carousel4, carousel5];
+    const poster = [carousel1, carousel2, carousel3, carousel4, carousel5, carousel6, carousel7, carousel8];
     const Responsive = {
         superLargeDesktop: {
             breakpoint: { max: 4000, min: 3000 },
